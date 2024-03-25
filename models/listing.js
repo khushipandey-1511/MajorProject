92da5329a622d97eed315993f467806dbb629770
Khushi Pandey <khushipandey151105@gmail.com>
2024-03-25 19:54:01 +0530

    updated

--- models/listing.js
+++ models/listing.js
@@ -9,8 +9,8 @@ const listingSchema= new Schema({
     description:String,
     image :{
         type: String,
-        default: "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" ,
-        set:(v)=>v===" "?"https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60":v,
+        default: "https://unsplash.com/photos/photography-of-brown-high-rise-building-beside-seashore-during-daytime-b8kEUZqMNoQ" ,
+        set:(v)=>v===""?"https://unsplash.com/photos/photography-of-brown-high-rise-building-beside-seashore-during-daytime-b8kEUZqMNoQ":v,
 
     },
     price: Number,
