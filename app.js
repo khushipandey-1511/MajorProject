9322fe5e34d94110f396cf1a249de4eb057b5e02
Khushi Pandey <khushipandey151105@gmail.com>
2024-03-27 12:25:12 +0530

    updated

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing=require("./models/listing.js")
 const path = require("path");
 const methodOvveride = require("method-override"); 
 const ejsMate=require("ejs-mate");
+const wrapAsync = require("./utilis/wrapAsync.js");
+
 let MONGO_URL="mongodb://127.0.0.1:27017/wonderlust";
 main().then(()=>{
 console.log("connected to DB")
@@ -25,19 +27,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 app.get("/",(req,res)=>{
     res.send("hi i am root")
 })
-// app.get("/testListing",async (req,res)=>{
-// let sampleListing = new Listing({
-//     title:"My new Villa",
-//     description:"BY the beach",
-//     price:1200,
-//     location:"Calangute,Goa",
-//     country:"India"
-// });
-//  await sampleListing.save();
-//  console.log(" sample was saved");
-//  res.send("successfull test")
 
-// })
 //index route
 app.get("/listings", async(req,res)=>{
    const allListings=await Listing.find({});
@@ -58,13 +48,13 @@ app.get("/listings", async(req,res)=>{
         res.render("listings/show.ejs",{listing})
     })
         //create route
-        app.post("/listings",async(req,res)=>{
-            // let {title,description,image,price,loaction,country}=req.body;
-           const newListing=  new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
+        app.post("/listings",wrapAsync(async(req,res,next)=>{
         
-        });
+          
+                const newListing=  new Listing(req.body.listing);
+                await newListing.save();
+                res.redirect("/listings");
+          }));
 
 
 //update route
@@ -86,6 +76,23 @@ app.delete("/listings/:id",async(req,res)=>{
     let deleteListing= await Listing.findByIdAndDelete(id);
     console.log(deleteListing);
     res.redirect("/listings")
+})
+// app.get("/testListing",async (req,res)=>{
+// let sampleListing = new Listing({
+//     title:"My new Villa",
+//     description:"BY the beach",
+//     price:1200,
+//     location:"Calangute,Goa",
+//     country:"India"
+// });
+//  await sampleListing.save();
+//  console.log(" sample was saved");
+//  res.send("successfull test")
+
+// });
+
+app.use((err,req,res,next)=>{
+    res.send("Something went wrong");
 })
 app.listen(8080,()=>{
     console.log("server is listening on port :8080")
--- init/index.js
+++ init/index.js
@@ -13,7 +13,7 @@ async function main(){
 }
 
 const initDB = async() =>{
-    await Listing.deleteMany({}); 
+    await Listing.deleteOne({}); 
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
